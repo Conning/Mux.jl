2d874b5ef8b1278ec5cf223599006cabb5ee34fc
Keith Mason <keith.mason@conning.com>
2019-03-06 11:53:41 -0500

    missed a line

--- src/Mux.jl
+++ src/Mux.jl
@@ -32,6 +32,6 @@ include("examples/basic.jl")
 include("examples/files.jl")
 
 defaults = stack(todict, basiccatch, splitquery, toresponse, assetserver, pkgfiles)
-wdefaults = stack(todict, wcatch, splitquery)
+#wdefaults = stack(todict, wcatch, splitquery)
 
 end
